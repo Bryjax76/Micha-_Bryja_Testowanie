e5f94ce6a20b8c56fbe4d66cb8eaac6131214528
Bryjax76 <66446836+Bryjax76@users.noreply.github.com>
2022-03-23 11:35:11 +0100

    Delete ImieNazwisko directory

--- ImieNazwisko/imie.py
+++
@@ -1,9 +0,0 @@
-class Pracownik():
-
-
-	def email(imie,nazwisko):
-		return imie + "." + nazwisko + "@testemail.com"
-	def nazwa(imie,nazwisko):
-		return imie + " " + nazwisko
-	def podwyzka(wynagrodzenie):
-		return wynagrodzenie * 2
